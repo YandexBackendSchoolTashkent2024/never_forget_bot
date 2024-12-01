1c9a8e7e9e107b00f27888420adf318870db0d8e
tiescl <anvar.madvaliev@icloud.com>
2024-12-02 04:05:09 +0500

    feat: add basic command and message handlers

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,44 @@
-#include <userver/clients/http/component.hpp>
+#include <cstdlib>
 #include <userver/components/minimal_server_component_list.hpp>
-#include <userver/server/handlers/ping.hpp>
-#include <userver/server/handlers/tests_control.hpp>
-#include <userver/testsuite/testsuite_support.hpp>
+#include <userver/storages/postgres/component.hpp>
+#include <userver/clients/http/component.hpp>
 #include <userver/utils/daemon_run.hpp>
 #include <tgbot/tgbot.h>
 
-#include "hello.hpp"
-
-using namespace std;
-using namespace TgBot;
-
-void startLongPolling(TgBot::Bot& bot) {
-    try {
-        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-        bot.getApi().deleteWebhook();
-        bot.getApi().setMyShortDescription("An awesome bot to make you forget about forgetting");
-
-        TgLongPoll longPoll(bot);
-        while (true) {
-            printf("Long poll started\n");
-            longPoll.start();
-        }
-    } catch (std::exception& e) {
-        printf("error: %s\n", e.what());
-    }
-}
+#include "bot/handlers/command_handlers.hpp"
+#include "bot/utils/utils.hpp"
 
 int main(int argc, char* argv[]) {
   auto component_list = userver::components::MinimalServerComponentList()
-                            .Append<userver::server::handlers::Ping>()
-                            .Append<userver::components::TestsuiteSupport>()
-                            .Append<userver::components::HttpClient>()
-                            .Append<userver::server::handlers::TestsControl>();
+                  .Append<userver::components::Postgres>("postgres-db-1")
+                  .Append<userver::components::HttpClient>();
 
-  never_forget_bot::AppendHello(component_list);
-
-
-  string token = "5899644754:AAGDugrRiYvH4RvWsJkimfC0ut0mq0HuviY";
+  std::string token(getenv("TOKEN"));;
   printf("Token: %s\n", token.c_str());
 
-  Bot bot(token);
+  TgBot::Bot bot(token);
 
-  bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
-      bot.getApi().sendMessage(message->chat->id, "welcome");
+  bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+      NeverForgetBot::Commands::onStartCommand(message, bot);
   });
 
-  bot.getEvents().onCommand("help", [&bot](Message::Ptr message) {
-      bot.getApi().sendMessage(message->chat->id, "help");
+  bot.getEvents().onCommand("help", [&bot](TgBot::Message::Ptr message) {
+      NeverForgetBot::Commands::onHelpCommand(message, bot);
   });
 
-  std::thread botThread(startLongPolling, std::ref(bot));
+  bot.getEvents().onUnknownCommand([&bot](TgBot::Message::Ptr message) {
+      bot.getApi().sendMessage(message->chat->id, "Invalid command");
+  });
+
+  bot.getEvents().onNonCommandMessage([&bot](TgBot::Message::Ptr message) {
+      bot.getApi().sendMessage(message->chat->id, "You typed: " + message->text);
+  });
+
+  std::thread botThread(NeverForgetBot::Utils::startLongPolling, std::ref(bot));
 
   userver::utils::DaemonMain(argc, argv, component_list);
 
   botThread.join();
+
+  return 0;
 }
