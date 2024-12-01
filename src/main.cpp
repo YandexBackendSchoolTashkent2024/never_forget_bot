f64b36efda27d374b920a4adc16181b118720450
tiescl <anvar.madvaliev@icloud.com>
2024-12-01 19:58:23 +0500

    feat: add tgbot to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,28 @@
 #include <userver/server/handlers/tests_control.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
 #include <userver/utils/daemon_run.hpp>
+#include <tgbot/tgbot.h>
 
 #include "hello.hpp"
+
+using namespace std;
+using namespace TgBot;
+
+void startLongPolling(TgBot::Bot& bot) {
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        bot.getApi().deleteWebhook();
+        bot.getApi().setMyShortDescription("An awesome bot to make you forget about forgetting");
+
+        TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    } catch (std::exception& e) {
+        printf("error: %s\n", e.what());
+    }
+}
 
 int main(int argc, char* argv[]) {
   auto component_list = userver::components::MinimalServerComponentList()
@@ -14,7 +34,25 @@ int main(int argc, char* argv[]) {
                             .Append<userver::components::HttpClient>()
                             .Append<userver::server::handlers::TestsControl>();
 
-  pg_service_template::AppendHello(component_list);
+  never_forget_bot::AppendHello(component_list);
 
-  return userver::utils::DaemonMain(argc, argv, component_list);
+
+  string token = "5899644754:AAGDugrRiYvH4RvWsJkimfC0ut0mq0HuviY";
+  printf("Token: %s\n", token.c_str());
+
+  Bot bot(token);
+
+  bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
+      bot.getApi().sendMessage(message->chat->id, "welcome");
+  });
+
+  bot.getEvents().onCommand("help", [&bot](Message::Ptr message) {
+      bot.getApi().sendMessage(message->chat->id, "help");
+  });
+
+  std::thread botThread(startLongPolling, std::ref(bot));
+
+  userver::utils::DaemonMain(argc, argv, component_list);
+
+  botThread.join();
 }
