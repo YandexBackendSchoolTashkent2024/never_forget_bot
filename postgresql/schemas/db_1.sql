8bcadcd9853ec78abd5130853826fa1b9020281d
tiescl <anvar.madvaliev@icloud.com>
2024-12-01 23:05:53 +0500

    fix: remove db creation in schemas file

--- postgresql/schemas/db_1.sql
+++ postgresql/schemas/db_1.sql
@@ -1,7 +1,3 @@
-DROP DATABASE IF EXISTS never_forget_bot_db_1;
-
-CREATE DATABASE IF NOT EXISTS never_forget_bot_db_1;
-
 DROP SCHEMA IF EXISTS never_forget_bot_schema CASCADE;
 
 CREATE SCHEMA IF NOT EXISTS never_forget_bot_schema;
