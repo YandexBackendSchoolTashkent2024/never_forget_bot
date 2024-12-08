537fd9edaa1ec20a56fefa7241a6c2172f810a28
Shokhrukh Safarov <safarovshohruh2803@gmail.com>
2024-12-08 19:33:22 +0500

    removed comments

--- src/main.cpp
+++ src/main.cpp
@@ -67,25 +67,7 @@ int main() {
     bot.getEvents().onCommand("upcoming_events", [&bot, &db](TgBot::Message::Ptr message) {
         long telegram_id = message->from->id;
 
-        // std::vector<NeverForgetBot::Event> events = db.getEventsOrderedByTimeDesc(bot, telegram_id);
         db.getEventsOrderedByTimeDesc(bot, telegram_id);
-
-        // if (events.empty()) {
-        //     bot.getApi().sendMessage(message->chat->id, "No events found.");
-        //     return;
-        // }
-
-        // std::string response = "Your Events:\n\n";
-        // for (const auto& event : events) {
-        //     response += "ID: " + event.id + "\n";
-        //     response += "Name: " + event.name + "\n";
-        //     response += "Time: " + event.time + "\n";
-        //     response += "Status: " + event.status + "\n";
-        //     response += "Created At: " + event.createdAt + "\n";
-        //     response += "Updated At: " + event.updatedAt + "\n\n";
-        // }
-
-        // bot.getApi().sendMessage(message->chat->id, response);
     });
 
     bot.getEvents().onCommand("notify", [&bot](TgBot::Message::Ptr message) {
