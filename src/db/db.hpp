c884d980faecd1b006b9f55897ba12f26b389553
izzattillakhodza <izzattillakhodza.m@payme.uz>
2024-12-08 18:04:39 +0500

    added command for change tz

--- src/db/db.hpp
+++ src/db/db.hpp
@@ -13,7 +13,7 @@ public:
     ~Database();
 
     std::optional<std::string> insertUser(long telegram_id, const std::optional<std::string>& username, const std::optional<std::string>& name);
-
+    bool updateUserTimeZone(long telegram_id, const std::string& timezone);
 private:
     pqxx::connection* conn;
 };
