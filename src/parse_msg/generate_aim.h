4a94ffee6d7f9b10b01e1ae6f4c5a2b8555873b7
tilavov-dostonbek <tilavov.dostonbek@gmail.com>
2024-12-08 17:31:17 +0500

    added YandexGPT integration

---
+++ src/parse_msg/generate_aim.h
@@ -0,0 +1,10 @@
+#ifndef GENERATE_AIM_H
+#define GENERATE_AIM_H
+
+#include <string>
+#include <curl/curl.h>
+
+// Function to generate the AIM token
+std::string generateAIMToken();
+
+#endif // GENERATE_AIM_H
