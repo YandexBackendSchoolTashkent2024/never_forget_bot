c32e1a87a8c3bdbd81a97f59ec7a609565bc43c9
tiescl <anvar.madvaliev@icloud.com>
2024-12-09 19:35:24 +0500

    fix: minor changes to db header

--- src/db/db.hpp
+++ src/db/db.hpp
@@ -7,10 +7,10 @@
 #include <string>
 #include <optional>
 #include <type_traits>
-#include <exception>
 
 #include "../models/event.hpp"
 #include "../models/notification.hpp"
+#include "../bot/handlers/events/events.hpp"
 #include "../bot/handlers/notifications/notifications.hpp"
 
 namespace NeverForgetBot {
@@ -21,7 +21,7 @@ public:
 
     ~Database();
 
-    std::vector<std::tuple<std::string, std::string, std::string, std::string>> fetchPendingNotifications();
+    std::vector<std::vector<std::string>> fetchPendingNotifications();
 
     void updateSentTimeForNotification(const std::string& notification_id);
 
@@ -35,16 +35,14 @@ public:
     std::optional<std::string> insertNotification(const std::string& event_id, const std::string& notification_time);
 
     void updateUserTimeZone(long telegram_id, const int& offset);
-  
+
     int getUserTimeZone(long telegram_id);
-  
+
     std::vector<Event> getEventsOrderedByTimeDesc(long telegram_id);
 
     Notification delayNotification(const std::string &notification_id, const std::string &interval);
 
     std::optional<std::string> changeEventStatus(const std::string &notification_id, const std::string &action);
-
-    std::optional<std::string> deleteEvent(long telegram_id, const std::optional<std::string>& username, const std::optional<std::string>& name);
 
     std::optional<std::string> getUserIdByTelegramId(long telegram_id);
 private:
