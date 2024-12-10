b1d0a7b8c6cd350a059d08093c4b5d3f29e7785b
tilavov-dostonbek <tilavov.dostonbek@gmail.com>
2024-12-10 16:36:10 +0500

    translated texts to russian

--- src/bot/utils/utils.cpp
+++ src/bot/utils/utils.cpp
@@ -80,16 +80,19 @@ std::optional<std::string> formatTimeWithTimezone(long telegram_id, const std::s
         ss >> std::get_time(&timeStruct, "%Y-%m-%d %H:%M:%S");
         if (ss.fail()) {
             throw std::runtime_error("Ошибка при разборе временной метки");
+            throw std::runtime_error("Ошибка при разборе временной метки");
         }
 
         time_t timeEpoch = std::mktime(&timeStruct);
         if (timeEpoch == -1) {
+            throw std::runtime_error("Ошибка преобразования времени в time_t");
             throw std::runtime_error("Ошибка преобразования времени в time_t");
         }
         timeEpoch += timezone * 3600;
 
         std::tm* updatedTimeStruct = std::gmtime(&timeEpoch);
         if (!updatedTimeStruct) {
+            throw std::runtime_error("Ошибка преобразования времени в структуру tm");
             throw std::runtime_error("Ошибка преобразования времени в структуру tm");
         }
 
@@ -99,8 +102,6 @@ std::optional<std::string> formatTimeWithTimezone(long telegram_id, const std::s
     } catch (const std::exception& e) {
         std::cerr << "Error formatting time: " << e.what() << std::endl;
         return std::nullopt;
-        std::cerr << "Ошибка форматирования времени: " << e.what() << std::endl;
-        return "Некорректное время";
     }
 }
 
